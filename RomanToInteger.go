44c9c3b6823c5fbb3a3b4336ddbff03085bd7d5b
jim.liu <jim.liu@numiner.io>
2023-06-05 17:47:52 +0800

    d

---
+++ RomanToInteger.go
@@ -0,0 +1,20 @@
+// 宣告程式屬於哪個 package
+package main
+
+// 引入套件
+import (
+    "fmt"
+)
+
+
+// 程式執行入口
+func main() {
+	fmt.Println(romanToInt("Jim"))
+}
+
+
+func romanToInt(s string) int {
+    var result int
+    // m := make(map[string]string)
+    return 1+result
+}
